829c38a95e5bd9958f263711bf4667680b521738
Sergio Garcia <sergio.gl86@gmail.com>
2021-11-19 15:40:39 +0100

    Creada la clase CrearRobot

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var names = [];
+var createName;
+createName = function () {
+    var letras = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+    var letra1 = letras.charAt(Math.floor(Math.random() * letras.length));
+    var letra2 = letras.charAt(Math.floor(Math.random() * letras.length));
+    var letra3 = letras.charAt(Math.floor(Math.random() * letras.length));
+    var numeroNombre = Math.floor(Math.random() * 100);
+    var redondeo = Math.round(numeroNombre);
+    var name = "".concat(letra1).concat(letra2).concat(letra3).concat(redondeo);
+    if (names.includes(name)) {
+        createName();
+    }
+    else {
+        names.push(name);
+        return name;
+    }
+};
 var typeRobot;
 (function (typeRobot) {
     typeRobot[typeRobot["Cleaner"] = 0] = "Cleaner";
@@ -10,5 +43,47 @@ var CreateRobot = /** @class */ (function () {
         this.tipo = tipo;
         this.bateria = bateria;
     }
+    CreateRobot.prototype.reset = function () {
+        this.bateria = 100;
+        this.nombre = createName();
+    };
     return CreateRobot;
 }());
+var Cleaner = /** @class */ (function (_super) {
+    __extends(Cleaner, _super);
+    function Cleaner(nombre) {
+        var _this = _super.call(this, nombre, typeRobot.Cleaner, 100) || this;
+        _this.nombre = nombre;
+        return _this;
+    }
+    Cleaner.prototype.trabajar = function () {
+        console.log("Larala larita, I clean my little house");
+    };
+    return Cleaner;
+}(CreateRobot));
+var Waiter = /** @class */ (function (_super) {
+    __extends(Waiter, _super);
+    function Waiter(nombre) {
+        var _this = _super.call(this, nombre, typeRobot.Waiter, 100) || this;
+        _this.nombre = nombre;
+        return _this;
+    }
+    Waiter.prototype.trabajar = function () {
+        console.log("Do you feel like a mini of fuet?");
+    };
+    return Waiter;
+}(CreateRobot));
+var Developer = /** @class */ (function (_super) {
+    __extends(Developer, _super);
+    function Developer(nombre) {
+        var _this = _super.call(this, nombre, typeRobot.Developer, 100) || this;
+        _this.nombre = nombre;
+        return _this;
+    }
+    Developer.prototype.trabajar = function () {
+        console.log("JavaScript is cool -> I develop with JavaScript -> I'm cool");
+    };
+    return Developer;
+}(CreateRobot));
+var Sergio = new Developer(createName());
+console.log(Sergio);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
+let names: String[] = [];
+
+let createName: () => String;
+
+createName = () => {
+  let letras = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+  let letra1 = letras.charAt(Math.floor(Math.random() * letras.length));
+  let letra2 = letras.charAt(Math.floor(Math.random() * letras.length));
+  let letra3 = letras.charAt(Math.floor(Math.random() * letras.length));
+
+  let numeroNombre = Math.floor(Math.random() * 100);
+  let redondeo = Math.round(numeroNombre);
+
+  let name: String = `${letra1}${letra2}${letra3}${redondeo}`;
+
+  if (names.includes(name)) {
+    createName();
+  } else {
+    names.push(name);
+    return name;
+  }
+};
+
 enum typeRobot {
   Cleaner,
   Waiter,
@@ -15,4 +38,43 @@ class CreateRobot implements robot {
     public tipo: typeRobot,
     public bateria: Number
   ) {}
+
+  reset() {
+    this.bateria = 100;
+    this.nombre = createName();
+  }
 }
+
+class Cleaner extends CreateRobot {
+  constructor(public nombre: String) {
+    super(nombre, typeRobot.Cleaner, 100);
+  }
+
+  trabajar() {
+    console.log("Larala larita, I clean my little house");
+  }
+}
+
+class Waiter extends CreateRobot {
+  constructor(public nombre: String) {
+    super(nombre, typeRobot.Waiter, 100);
+  }
+
+  trabajar() {
+    console.log("Do you feel like a mini of fuet?");
+  }
+}
+
+class Developer extends CreateRobot {
+  constructor(public nombre: String) {
+    super(nombre, typeRobot.Developer, 100);
+  }
+
+  trabajar() {
+    console.log("JavaScript is cool -> I develop with JavaScript -> I'm cool");
+  }
+}
+
+let Sergio = new Developer(createName());
+
+console.log(Sergio);
